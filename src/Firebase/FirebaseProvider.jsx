7881182cc2e4bf821bcdf64342b77bba1c7ec579
MD-Kayesur <rmdkayesur@gmail.com>
2025-08-02 19:32:01 +0600

    install firebase but didnt connent with my site

---
+++ src/Firebase/FirebaseProvider.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBOUTxFvKLK4Y7GoA-cxRxQVMtRnSmUH7w",
+  authDomain: "md-kayesur.firebaseapp.com",
+  projectId: "md-kayesur",
+  storageBucket: "md-kayesur.firebasestorage.app",
+  messagingSenderId: "1062026936813",
+  appId: "1:1062026936813:web:b39b0680393181148027e4"
+};
+
+// Initialize Firebase
+ export const app = initializeApp(firebaseConfig);
